1aa8044cb2207f67d5356ee2d6de734ce0aa7825
smalameen <smalameen0@gmail.com>
2021-01-05 12:50:16 +0600

    image works

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qsyrk.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('doctors'));
+app.use(fileUpload());
 
 const port = 5001;
 
@@ -35,6 +38,38 @@ client.connect(err => {
       })
       
   })
+
+
+  app.get ('/findUserByMail' ,  (req, res) => {
+    const mail = req.body;
+    console.log(mail);
+    
+    dataCollection.find()
+
+    .toArray((err, documents) => {
+        res.status(200).send(documents);
+    })
+
+})
+
+
+app.post ('/addCourse' ,  (req, res) => {
+    const files = req.files.file;
+    const pass = req.body.pass;
+    const email = req.body.email;
+    console.log(files, pass, email);
+
+    files.mv(`${__dirname}/course/${files.name}`, error => {
+    if(error){
+    console.log(error);
+    return res.status(5001).send({meg:"Failed to load image"})
+}
+    })
+    return res.send({name: files.name, path:`/${files.name}`})
+
+
+   
+});
  
 });
 
